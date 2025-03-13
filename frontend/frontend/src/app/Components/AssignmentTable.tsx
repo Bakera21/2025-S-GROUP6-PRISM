b3c5f6919c8b5e2fd70099b49f240bed92534fc5
Alexander Baker <bakera21@unlv.nevada.edu>
2025-03-13 16:05:29 -0700

    Implemented a dropdown with text input

---
+++ frontend/frontend/src/app/Components/AssignmentTable.tsx
@@ -0,0 +1,22 @@
+"use client";
+// Using Autocomplete from Material UI
+// Documentation here: https://mui.com/material-ui/react-autocomplete/
+
+import * as React from "react";
+import { useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+//interface Props {
+//  items: { Label: string; id: number }[];
+//  onSelectItem: (item: number | null) => void;
+//}
+
+export default function AssignmentTable() {
+  return <></>;
+}
